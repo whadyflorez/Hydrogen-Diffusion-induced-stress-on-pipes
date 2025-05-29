f80c3fd7541d65ebbf97434949c785c3d8fb46ed
Whady Felipe Florez <whady.florez@upb.edu.co>
2025-05-29 08:07:31 -0500

    Update pore_model.py

--- pore_model.py
+++ pore_model.py
@@ -24,8 +24,8 @@ b=0.02661*1e-3 #H2 m**3/mol
 E=0.565e9 #elastic modulus HDPE Pa
 nu=0.4  #poisson ration HDPE
 Sc_t=24*3600 #time scale days
-t_end=3000 #days
-dt=0.1 #days
+t_end=360*10 #days
+dt=0.025 #days
 n_step=int(t_end/dt)
 
 n=np.zeros(n_step+1) #moles inside 
@@ -46,16 +46,17 @@ def SF(D,z):
     s=2*pi*D/(1-D/(4*z))
     return s
 
-#state equation for pressure @V,t=constant
+#staate equation for pressure @V,t=constant
 def P_se(n,V,T):
-    P=n*Ru*T/V 
+#    P=n*Ru*T/V #ideal gas
+    P=n*Ru*T/(V-n*b)-a*n**2/V**2 #Van der walls
     return P
 
 #time steps
 r[0]=ro_pore
 n_in=flux_pipe(r_pos)
 V[0]=4.0/3.0*pi*r[0]**2
-S1=SF(2*r[0],r_pos-ri)
+S1=SF(2*r[0],r_pos-ri)*0
 S2=SF(2*r[0],ro-r_pos)
 for j in range(n_step):
     denom=1.0+dt*Sc_t*(0.5*S1*Dif*1.0/V[j]+0.5*S2*Dif*1.0/V[j])
@@ -67,8 +68,8 @@ for j in range(n_step):
     V[j+1]=4.0/3.0*pi*r[j+1]**2
     P[j+1]=P_se(n[j+1],V[j+1],T) 
     c_pore[j+1]=n[j+1]/V[j+1]
-    S1=SF(2*r[j+1],r_pos-ri)
-    S2=SF(2*r[j+1],ro-r_pos)        
+    S1=SF(2*r[j+1],r_pos-ri)*0
+    S2=SF(2*r[j+1],ro-r_pos)       
 
 #moles
 fig, ax = plt.subplots()
